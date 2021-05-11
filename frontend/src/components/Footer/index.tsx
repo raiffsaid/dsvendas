4e25ee7b8d172dd3cd33f3e2e85e10513db412f4
Raiff Smith Said <raiff.said@gmail.com>
2021-05-11 03:49:46 -0300

    feat: created NavBar and Footer components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/raiffsaid" target="_blank" rel="noreferrer">Raiff Said</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
